6bffafbc95b599b2f289245e9bd8c886b8e8ec3b
Leonardo <accs.lndrs@gmail.com>
2023-08-25 13:11:14 -0300

    .

--- controllers/person_controller.go
+++ controllers/person_controller.go
@@ -117,19 +117,8 @@ func (pc *PersonController) CreatePerson(w http.ResponseWriter, r *http.Request)
 
 	person.ID = db.CustomUUID(generatedUUID.String())
 
-	defer func() {
-		if r := recover(); r != nil {
-			pc.personRepository.InsertPerson(person)
-			pc.queue.Enqueue([]*models.Person{person})
-			return
-		}
-	}()
-
-	success := pc.queue.SendToMonitor(person)
-	if !success {
-		pc.personRepository.InsertPerson(person)
-		pc.queue.Enqueue([]*models.Person{person})
-	}
+	go pc.personRepository.InsertPerson(person)
+	go pc.queue.Enqueue([]*models.Person{person})
 
 	w.Header().Set("Location", fmt.Sprintf("/pessoas/%s", person.ID))
 	w.WriteHeader(http.StatusCreated)
